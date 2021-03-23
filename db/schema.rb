bec9092b0e4abca53ca4ee79135714b92d70fe3c
kedeinroga <keybroga@gmail.com>
2021-03-22 21:25:44 -0500

    created jointable course to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_23_021229) do
+ActiveRecord::Schema.define(version: 2021_03_23_022444) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,11 @@ ActiveRecord::Schema.define(version: 2021_03_23_021229) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["category_id"], name: "index_courses_on_category_id"
+  end
+
+  create_table "courses_users", id: false, force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "course_id", null: false
   end
 
   create_table "users", force: :cascade do |t|
